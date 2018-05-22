b5deae301590c72759d2de5d92bbee071638fe3c
coolstone <coolstoneyoung@gmail.com>
2018-05-22 17:44:49 +0800

    修改测试

--- src/main/java/org/coolstone/App.java
+++ src/main/java/org/coolstone/App.java
@@ -4,10 +4,11 @@ package org.coolstone;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!中文" );
+        System.out.println("go");
     }
 }
